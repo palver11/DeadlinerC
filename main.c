ef17d71ec9d38224fb93dbdae0d6450b7c7658ea
Palver <63277841+palver11@users.noreply.github.com>
2024-08-13 01:54:17 +0800

    developing brrr

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 // TODO
 // move constants from the function into the global scope
 // add 0s in singular values when printing time
+// check why program doesn't show deadline for tasks array index 1
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
@@ -10,31 +12,32 @@ void show_tasks(int* tasks);
 void set_task(int* tasks);
 void conv_secs(int secs);
 
+const int SEC_DAY = 86400;
+const int SEC_HR = 3600;
+const int SEC_MIN = 60;
+
 int tasks[2];
 
 int main(int argc, char* argv[]) {
 
    set_task(tasks);
-   printf("\nEXECUTED"); // DEBUG
    show_tasks(tasks);
    
-
-
    return 0;
 }
 
-void show_tasks(int* tasks) {
+void show_tasks(int tasks[]) {
    // Get the current time
    time_t now = time(NULL);
 
    for (int i = 0; tasks[i] != 0; i++) {
-      printf("\nTask %d: ", i+1);
+      printf("\n= Task %d =", i+1);
       // Calculate the difference in seconds between the current time and the future time
       conv_secs(difftime(tasks[i], now));
    }
 }
 
-void set_task(int* tasks) {
+void set_task(int tasks[]) {
    int index;
    int year;
    int mon;
@@ -42,27 +45,20 @@ void set_task(int* tasks) {
    int hour;
    int mins;
    // int secs;
+
    // User Input
    printf("\nChoose task to change by index: ");
-   scanf("%d", index);
+   scanf("%d", &index);
    printf("\nEnter end time of your deadline (nums).\nYear: ");
-   scanf("%d", year);
+   scanf("%d", &year);
    printf("\nMonth: ");
-   scanf("%d", mon);
+   scanf("%d", &mon);
    printf("\nDay: ");
-   scanf("%d", day);
+   scanf("%d", &day);
    printf("\nHour: ");
-   scanf("%d", hour);
+   scanf("%d", &hour);
    printf("\nMinutes: ");
-   scanf("%d", mins);
-
-   // DEBUG
-   printf("\n%d", index);
-   printf("\n%d", year);
-   printf("\n%d", mon);
-   printf("\n%d", day);
-   printf("\n%d", hour);
-   printf("\n%d", mins);
+   scanf("%d", &mins);
 
    // Define a struct tm for a future date (e.g., January 1, 2030)
    struct tm future_date = {0};
@@ -75,6 +71,7 @@ void set_task(int* tasks) {
 
    // Convert the future date to a time_t value
    time_t future_time = mktime(&future_date);
+      // Error check
    if (future_time == -1) {
       printf("Failed to convert future date.\n");
       return;
@@ -90,17 +87,11 @@ void conv_secs(int secs) {
       return;
    }
 
-   // Convert seconds to days
-   const int SEC_DAY = 86400;
-   const int SEC_HR = 3600;
-   const int SEC_MIN = 60;
-
    int days;
    int hrs;
    int mins;
 
-   printf("%d", secs);
-
+   // Convert seconds to days
    if (secs >= SEC_DAY)
       days = secs / SEC_DAY;
       secs -= days * SEC_DAY;
